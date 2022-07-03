14ea1ead0080d07a5d1bf89a80423cdaa8d89b2e
AliOlliek <olliek95@gmail.com>
2022-07-03 17:32:37 +0300

    Finished Styling of TopBar

--- react-front-end/src/components/Landing/TopBar/UserSection/UserSection.js
+++ react-front-end/src/components/Landing/TopBar/UserSection/UserSection.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import '../../../../stylesheets/TopBar/userSection.css'
+import globe from '../../../../assets/images/globe-icon.png'
+import user from '../../../../assets/images/user.png'
+import menu from '../../../../assets/svgs/menu.svg'
 
 export default function UserSection() {
   return (
     <>
-      <div>
+      <div className="userSection">
         <button>Become a Host</button>
-        <button>Languages icon</button>
-        <button>User Menu</button>
+        <button><img src={globe} alt='' />
+        </button>
+        <div className='userMenu'>
+          <img src={menu} alt=''/>
+          <img src={user} alt=''/>
+        </div>
       </div>
     </>
   );
