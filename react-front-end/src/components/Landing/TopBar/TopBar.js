b901f460622dd28afa2b26c1a74cea5963ee9768
AliOlliek <olliek95@gmail.com>
2022-07-03 18:21:25 +0300

    Adjusted Logo and Spacing in Top Bar

--- react-front-end/src/components/Landing/TopBar/TopBar.js
+++ react-front-end/src/components/Landing/TopBar/TopBar.js
@@ -2,14 +2,12 @@ import React from 'react'
 import SearchBar from './SearchBar/SearchBar'
 import UserSection from './UserSection/UserSection'
 import "../../../stylesheets/TopBar/topBar.css";
-import logo from '../../../assets/svgs/airbnb.svg'
+import logo from '../../../assets/svgs/logo.svg'
 
 export default function TopBar() {
   return (
     <div className='topBar'>
-      <button>
-        <img src={logo} alt=''/>
-      </button>
+      <img src={logo} alt=''/>
       <SearchBar />
       <UserSection />
     </div>
