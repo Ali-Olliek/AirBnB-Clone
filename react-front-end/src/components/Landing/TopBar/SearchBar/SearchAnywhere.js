c31be543618a557dd8739fa98b4c70e66ddebcf0
AliOlliek <olliek95@gmail.com>
2022-07-04 08:49:02 +0300

    Added Regions Select to Search "Anywhere" Element

--- react-front-end/src/components/Landing/TopBar/SearchBar/SearchAnywhere.js
+++ react-front-end/src/components/Landing/TopBar/SearchBar/SearchAnywhere.js
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import  { Italy, France, ME, USA, World, SouthAmerica } from '../../../../assets/images/regions'
+import '../../../../stylesheets/TopBar/searchAnywhere.css'
 export default function SearchAnywhere() {
+  const regions = [
+    ["I'm flexible",World],
+    ["ME", ME],
+    ["Italy", Italy],
+    ["United States", USA],
+    ["France", France],
+    ["South America", SouthAmerica]
+  ]
+
   return (
-    <div>Anywhere</div>
-  )
+    <>
+      <div className="regions">
+        <h4>Search by region</h4>
+        <div className="regions-container">
+          {regions.map((region) => (
+            <button>
+              <img src={region[1]} alt="" />
+              <p>{region[0]}</p>
+            </button>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
