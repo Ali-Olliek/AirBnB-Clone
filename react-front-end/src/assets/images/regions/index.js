6139d80086bde2224dac2a542faa859c0d952947
AliOlliek <olliek95@gmail.com>
2022-07-04 08:48:06 +0300

    Added Region Images to Assets and Exported Them

---
+++ react-front-end/src/assets/images/regions/index.js
@@ -0,0 +1,10 @@
+import Italy from './Italy.jpg'
+import France from './France.jpg'
+import ME from './ME.jpg'
+import USA from './USA.jpg'
+import World from './world.jpg'
+import SouthAmerica from './SouthAmerica.jpg'
+
+export {Italy, France, ME, USA, World, SouthAmerica };
+
+// https://stackoverflow.com/a/55143345/18590539
